cb8d01b08c97a03cdaa519e288dc3576ac6a6b3b
ZohaMohsin <amnamohsin72@gmail.com>
2024-11-23 20:54:39 +0500

    act 2

---
+++ act 2.py
@@ -0,0 +1,15 @@
+class student:
+    grade="11"
+    name="penguin"
+
+    def intro(self):
+        print("Hi i am a student")
+
+    def show(self):
+        print("my name is ",self.name," and i study in grade ",self.grade)
+
+st=student()
+st.intro()
+st.show()
+
+    
